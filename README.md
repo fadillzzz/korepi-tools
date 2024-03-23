439216aedbcc4debcd2c3e7150535d4eac3357ec
Kemal Fadillah <fadillzzz@gmail.com>
2024-03-22 21:37:37 -0700

    feat(auth): Online auth bypass

--- README.md
+++ README.md
@@ -6,18 +6,35 @@ Some Korepi related stuff that I wrote while learning how to hack
 
 You'll want to have at least Node 18 or newer. Don't forget to install the dependencies with `npm i`.
 
-## How to Use
+## How to Use (Local auth)
 
 1. Launch Korepi once without any license, and it should give you a hardware ID.
 2. Then run the `license_gen.js` which will asks you for your HWID and generates a license file to use.
 3. **Copy** (don't cut) the license file into the same folder where the exe is located.
 4. Run `ssl_gen.js` and install the generated certificated (`certs/md5c.korepi.com.crt`) as a trusted root certificate.
 5. Edit your hosts file and add the following lines:
-
 ```
 127.0.0.1 md5c.korepi.com
 127.0.0.1 md5c.mxmicah.me
 ```
 6. Then start `server.js` and launch Korepi.
 
-For future launches, you only need to run the `server.js` file (no need to regenerate the license or SSL).
+## How to Use (Online auth)
+
+1. In the directory where Korepi executable is located, create a file named `license.json` and copy the following line into it:
+
+```
+{"license":"KOREPI-REAL-KEY"}
+```
+2. Run `ssl_gen.js` and install the generated certificated (`certs/md5c.korepi.com.crt`) as a trusted root certificate.
+3. Edit your hosts file and add the following lines:
+```
+127.0.0.1 auth.btxo.cn
+```
+4. Then start `server.js` and launch Korepi.
+
+**NOTE:** For future launches, you only need to run the `server.js` file (no need to regenerate the license or SSL).
+
+## Credits
+
+- Anonymous for providing a key for testing
--- server.js
+++ server.js
@@ -46,6 +46,75 @@ function getHash(payload) {
     return hash.update(restructured).digest('hex');
 }
 
+let encKey = '';
+let hwid = '';
+
+function getHmac(requestType, payload) {
+    let secret = 'aaa49c02ddae6a76e805e79e8d9bb788f3c80556b43b4a70fb3513bdc4e37454';
+
+    if (requestType !== 'init') {
+        secret = encKey + '-' + secret;
+    }
+
+    const hmac = crypto.createHmac("sha256", secret);
+    hmac.update(payload);
+    return hmac.digest("hex");
+}
+
+function getFakePayloadForOnlineAuth(type) {
+    let payload = {};
+
+    if (type === 'init') {
+        payload = {
+            success: true,
+            message: "Initialized",
+            sessionid: "FAKE_SESH",
+            appinfo: {
+                numUsers: "N/A - Use fetchStats() function in latest example",
+                numOnlineUsers: "N/A - Use fetchStats() function in latest example",
+                numKeys: "N/A - Use fetchStats() function in latest example",
+                version: "1.0",
+                customerPanelLink: "https://keyauth.cc/panel/Strigger/Korepi/"
+            },
+            newSession: false,
+            nonce: "12345678123456781234567812345678",
+        }
+    } else if (type === 'checkblacklist') {
+        payload = {
+            success: false,
+            message: 'Client is not blacklisted'
+        }
+    } else if (type === 'license') {
+        const now = Math.floor(Date.now() / 1000);
+        payload = {
+            success: true,
+            message: "Logged in!",
+            info: {
+                username: "KOREPI-REAL-KEY",
+                subscriptions: [
+                    {
+                        subscription: "Fans & Sponsor & Tester",
+                        key: "KOREPI-REAL-KEY",
+                        expiry: (Math.pow(2, 32) - 1).toString(),
+                        timeleft: (Math.pow(2, 32) - 1) - now,
+                        level: "25" // Pertamax?
+                    }
+                ],
+                ip: "127.0.0.1",
+                hwid,
+                createdate: now.toString(),
+                lastlogin: now.toString()
+            },
+            nonce: "12345678123456781234567812345678",
+        }
+
+    } else {
+        console.error("Unknown request type", type);
+    }
+
+    return payload;
+}
+
 const requestListener = function (req, res) {
     console.log(new Date().toISOString() + ': ' + req.headers.host + req.url);
 
@@ -84,6 +153,36 @@ const requestListener = function (req, res) {
             data: payload,
             signature
         }));
+    } else if (req.url.indexOf('/1.2/') !== -1) {
+        let body = '';
+
+        req.on('data', buffer => {
+            body += buffer.toString(); // convert Buffer to string
+        });
+
+        req.on('end', () => {
+            const matches = body.match(/type=(.+?)\&/);
+            console.log("Request type is", matches[1]);
+            const requestType = matches[1];
+
+            if (requestType === 'init') {
+                const matches = body.match(/enckey=(.+?)\&/);
+                encKey = matches[1];
+                console.log("Setting enckey to", encKey);
+            }
+
+            if (requestType === 'checkblacklist') {
+                const matches = body.match(/hwid=(.+?)\&/);
+                hwid = matches[1];
+            }
+
+            const fakePayload = getFakePayloadForOnlineAuth(requestType);
+
+            const fakeResponse = JSON.stringify(fakePayload);
+
+            res.setHeader("signature", getHmac(requestType, fakeResponse));
+            res.end(fakeResponse);
+        });
     } else {
         res.end("Hello world!");
     }
