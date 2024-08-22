6df32d460bf83e7b9f102f48e168e18c8bc0c7eb
Kemal Fadillah <fadillzzz@gmail.com>
2024-08-22 00:13:53 -0700

    DLL bypass update

--- lol/dll.cpp
+++ lol/dll.cpp
@@ -4,6 +4,7 @@
 #include "MinHook.h"
 #include "Sig.hpp"
 #include <format>
+#include <inttypes.h>
 #include <iostream>
 
 bool fakeResp = false;
@@ -13,6 +14,9 @@ callback_t callback = nullptr;
 
 typedef void (*options_t)(void *, size_t, void *);
 options_t oOptions = nullptr;
+
+typedef intmax_t (*strtoimax_t)(const char *nptr, char **endptr, int base);
+strtoimax_t oStrtoimax = nullptr;
 
 void options(void *a1, size_t a2, void *a3) {
     if (a2 == 10002) {
@@ -33,7 +37,7 @@ void options(void *a1, size_t a2, void *a3) {
 }
 
 const std::string resp =
-    R"({"msg": "vpJSftgQ2noDAZR3Iri/ForvdhDZvxwlJCXowV9TgKSs+BoMyBMOIuxjpDcMTSov1thaXhg/d9aAKcpxOP6glQ3bSd8bHIGMku3Ck/33VdYhtzx4HwC4Lel5mVGZ9+2jffsIgHyIwxMl+8kYwh/QGQRlkC8zFfyNaMszsZiOxIJCy/RMYfI3buvCDPH/4D1/VxysPnaX+QtrVrs7Bt74byqnd38bi0GhpllEWL7CO+7fI+vMe2OSv6s0CUaOqzhDC5N8wIkHsthyVyP+GYoltTov3Bu5iaxmgZc/eYQPTkTWQ759pIVNjKJwnQI3EtOEdrRog6LAkA/CMGwMwBkScvY508Z3KhnNqqIIF9RpYLI6rdST+o2t5gIK4sElQg/2wHZT6wSm23t7YdxnwzEFZysv/H0y63iI4NMUmyZIkRvCyxlWVMpTt/rV9qubdbCjGDxG7A/0LbxCJBfBgEWu4Krpp1S+hk4qgIB+2apCh5sxU76mLzQdFLzNrgmbQADapyDO6rWw777F9FKlo/r9II8kISi/+2FxXp7TZE3ALbcyUo7zKucahsq7u9ucENm64D3PKV4YZCHchQY7xyYI4DaC1PQzleJxGaGbCoBQ0PZK7f33d3N3qB10OaEfe2de4uTcOKbVAjtjSLrlZcMGiZd40Bho76xCtcgAKG2FDxbH/PJo4BoIYwqiDzqpmxXBOsn0JqKLGLaAyU840GAgyLO62lE7/A26w+B9q7hkOIcKlfXZpdwjsll/dADe2U/uF5nrLxEOUGDx9gbUoB95KLD1S3KCCyaLuv8j4imt2E9EgDzk/1XdIwnbPGAECajV5z4yTpMuyD9XBhmJQIFutw==", "code": 200})";
+    R"({"msg": "AtZAfcAb+qtSipkXI9CP8u5XUYPGyCbGq5C/VYyt6tcelFYehMuYs0q8m/q+RwGx0/jOB3jDRAqjcqmunJpoKrIFV9W/YC9wzY+GaSU2L8oNQHlpx9KgJ0K50aqwxQD0dKiWmd16b76sLCn8GvpVrSk1k6SoFtUtPe30Cf1BkOsFD2oxSGBioUK22MkPFO2uj5xIXfZ5tC1dB4cS5ttzVlDiLPXY1hlJBqgFpZTj8znRz5qpMhflK5euefmKRPTKzwt+JHFF2YImsmDf49bMCgS6ZIwHL/jbK8dRJwFRjfkZjvpw2XxrL3wKubLqZKjUG3lHP6oKijmcWFTeu68xHphRKmqy43Gg3MZ1wCoYwcQL6tPPoqMy6TJwJdt/mBfhklPRq0XcTAjpnTIJeIo7zH/L1kFaGRAVFtqbwGLIIN08bb+7/tV3MOOc8BEp4RCb721hakBRNFqJAeYrt7yzr/VeK2igLuByrTcBkd0SOIB5LgI5K/qrMf/90bB8sfcicIgJXSVxyuuov45UXM2Rdo4YiL5M8b4LCJwhEkmplS8=", "code": 200})";
 
 typedef size_t (*perform_t)(void *);
 perform_t oPerform = nullptr;
@@ -46,6 +50,25 @@ size_t perform(void *a1) {
     }
 
     return oPerform(a1);
+}
+
+intmax_t strtoint(const char *nptr, char **endptr, int base) {
+    if (memcmp(nptr, "1721968703399", 13) == 0) {
+        return 0x1f0ed5537a7;
+    }
+
+    return oStrtoimax(nptr, endptr, base);
+}
+
+char *base = nullptr;
+const auto expectedRegion = 0x3d3000;
+
+void initWrapper() {
+    const void *init = Sig::find(base, expectedRegion,
+                                 "48 89 5C 24 08 48 89 74 24 10 48 89 7C 24 18 4C 89 74 24 20 55 48 8D AC 24 10");
+
+    CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)init, nullptr, 0, nullptr);
+    ExitThread(0);
 }
 
 void start() {
@@ -72,8 +95,6 @@ void start() {
 
     MEMORY_BASIC_INFORMATION mbi;
     bool foundBase = false;
-    const auto expectedRegion = 0x3c7000;
-    char *base = nullptr;
 
     while (foundBase == false) {
         base = nullptr;
@@ -87,21 +108,46 @@ void start() {
         }
     }
 
+    // {
+    //     const void *found =
+    //         Sig::find(base, expectedRegion, "89 54 24 10 4C 89 44 24 18 4C 89 4C 24 20 48 83 EC 28 48 85 C9");
+
+    //     if (found != nullptr) {
+    //         MH_CreateHook((LPVOID)found, (LPVOID)options, (LPVOID *)&oOptions);
+    //         MH_EnableHook((LPVOID)found);
+    //     }
+    // }
+
+    // {
+    //     const void *found = Sig::find(base, expectedRegion, "40 55 56 48 83 EC 38 48 8B F1 48 85 C9 75 0A 8D");
+
+    //     if (found != nullptr) {
+    //         MH_CreateHook((LPVOID)found, perform, (LPVOID *)&oPerform);
+    //         MH_EnableHook((LPVOID)found);
+    //     }
+    // }
+
+    // {
+    //     MH_CreateHook((LPVOID)strtoimax, (LPVOID)strtoint, (LPVOID *)&oStrtoimax);
+    //     MH_EnableHook((LPVOID)strtoimax);
+    // }
+
     {
-        const void *found =
-            Sig::find(base, expectedRegion, "89 54 24 10 4C 89 44 24 18 4C 89 4C 24 20 48 83 EC 28 48 85 C9");
+        const void *found = Sig::find(base, expectedRegion, "83 3D ? ? ? ? 00 75 04 33 C9 CD 29");
 
         if (found != nullptr) {
-            MH_CreateHook((LPVOID)found, (LPVOID)options, (LPVOID *)&oOptions);
-            MH_EnableHook((LPVOID)found);
+            const auto relative = *(int32_t *)((uintptr_t)found + 2);
+            int32_t *role = (int32_t *)((uintptr_t)found + relative + 7);
+            *role = 31;
         }
     }
 
     {
-        const void *found = Sig::find(base, expectedRegion, "40 55 56 48 83 EC 38 48 8B F1 48 85 C9 75 0A 8D");
+        const void *found =
+            Sig::find(base, expectedRegion, "48 89 5C 24 ? 48 89 7C 24 ? 55 48 8D 6C 24 ? 48 81 EC C0 00 00 00");
 
         if (found != nullptr) {
-            MH_CreateHook((LPVOID)found, perform, (LPVOID *)&oPerform);
+            MH_CreateHook((LPVOID)found, (LPVOID)initWrapper, nullptr);
             MH_EnableHook((LPVOID)found);
         }
     }
